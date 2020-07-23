1ac5762ce2e9971a097e5261256137c776ecb857
Patrick Kissling <patrick.kissling@gmx.de>
2020-07-23 11:44:43 +0200

    Add CORS rules

--- bucket/attachments.tf
+++ bucket/attachments.tf
@@ -3,6 +3,16 @@ resource "aws_s3_bucket" "attachments" {
   bucket = "${var.domain}-attachments"
   acl    = "private"
 
+  cors_rule {
+    allowed_headers = ["Content-Type"]
+    allowed_methods = ["PUT"]
+    allowed_origins = [
+      "https://${var.domain}",
+      "https://www.${var.domain}"
+    ]
+    max_age_seconds = 3000
+
+  }
   # allow deletion of non-empty bucket
   force_destroy = true
 
